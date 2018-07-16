b197a233ea491f718c6f9b304477898f4a8225b1
hanseuljun <hanseuljun@gmail.com>
2018-07-16 04:46:44 -0700

    Fix order of quaternion conversion to match Unity.

--- python/test_unity_rotation.py
+++ python/test_unity_rotation.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 import unity_rotation
 
+def convert_words(words):
+    for i in range(len(words)):
+        words[i] = float(words[i])
+    return words
+
+def convert_line(line):
+    words = line.split('/')
+    quaternion = convert_words(words[0].split(','))
+    euler = convert_words(words[1].split(','))
+    look_at = convert_words(words[2].split(','))
+    look_up = convert_words(words[3].split(','))
+    return quaternion, euler, look_at, look_up
+
 def main():
     with open('../test_case.txt', 'r') as f:
         for line in f:
-            words = line.split('/')
-            quaternion = words[0].split(',')
-            euler = words[1].split(',')
-            qw, qx, qy, qz = unity_rotation.convert_euler_to_quaternion(float(euler[0]), float(euler[1]), float(euler[2]))
+            quaternion, euler, look_at, look_up = convert_line(line)
+            qw, qx, qy, qz = unity_rotation.convert_euler_to_quaternion(euler[0], euler[1], euler[2])
             print('quaternion: {}, {}, {}, {}'.format(quaternion[0], quaternion[1], quaternion[2], quaternion[3]))
             print('q: {}, {}, {}, {}'.format(qw, qx, qy, qz))
 
--- python/unity_rotation.py
+++ python/unity_rotation.py
@@ -13,7 +13,12 @@ def convert_euler_to_quaternion(pitch, yaw, roll):
     cr = math.cos(math.radians(roll) * 0.5)
 
     qw = cy * cr * cp + sy * sr * sp;
-    qx = cy * sr * cp - sy * cr * sp;
-    qy = cy * cr * sp + sy * sr * cp;
-    qz = sy * cr * cp - cy * sr * sp;
+    #qx = cy * sr * cp - sy * cr * sp;
+    #qy = cy * cr * sp + sy * sr * cp;
+    #qz = sy * cr * cp - cy * sr * sp;
+    # The order is different from the reference and maybe that's because
+    # Unity is based on a left-handed coordinate system?
+    qz = cy * sr * cp - sy * cr * sp;
+    qx = cy * cr * sp + sy * sr * cp;
+    qy = sy * cr * cp - cy * sr * sp;
     return qw, qx, qy, qz
